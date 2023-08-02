3dae36bef5b953f3702fcca91492435c8d2cccfc
Angelina <vitkovskaya.03@mail.ru>
2023-08-02 19:14:12 +0300

    Add authorization and registration UI

--- app/src/main/java/com/angelina/wallet_application/ui/theme/Type.kt
+++ app/src/main/java/com/angelina/wallet_application/ui/theme/Type.kt
@@ -2,33 +2,42 @@ package com.angelina.wallet_application.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.angelina.wallet_application.R
+
+val titleFontFamily = FontFamily(Font(R.font.inter_semi_bold))
+
+val textFontFamily = FontFamily(Font(R.font.inter_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+
+    //main text in screen
+    titleMedium = TextStyle(
+        fontSize = 30.sp,
+        fontFamily = titleFontFamily,
+        letterSpacing = (-0.3).sp
     ),
+
+    //subtext on screen
+    titleSmall = TextStyle(
+        fontSize = 17.sp,
+        fontFamily = textFontFamily
+    ),
+
+    //title in alert dialog
+    labelMedium = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = titleFontFamily,
+        letterSpacing = (-0.24).sp
+    ),
+
+    //subtext in alert dialog and textField title
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        fontFamily = textFontFamily,
     )
-    */
 )
